c0eeea1ebb39a926d2c81cbf0652876478a00b34
wang <wzy@impression.co.jp>
2017-12-14 15:41:10 +0900

    Gitのテスト

---
+++ myproj/src/hoge/Hello.java
@@ -0,0 +1,11 @@
+package hoge;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO王振友	
+        System.out.println("Hello王");
+        System.out.println("Hello");
+	}
+
+}
